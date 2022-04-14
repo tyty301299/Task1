4168a604dfcd69297ede72bb2f06c59e956be6fc
tynguyen <tyandlai12@gmail.com>
2022-04-14 10:26:37 +0700

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Nguyen Ty on 14/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
